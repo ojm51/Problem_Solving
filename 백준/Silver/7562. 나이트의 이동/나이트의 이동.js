cd65507364b7d2ac5bab45a385b84a2fabb961d2
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2025-12-30 12:23:23 +0900

    [Silver I] Title: 나이트의 이동, Time: 808 ms, Memory: 123944 KB -BaekjoonHub

---
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.js
@@ -0,0 +1,46 @@
+const input = require("fs").readFileSync('/dev/stdin').toString().trim().split('\n'); 
+const T = Number(input[0]);
+const answer = [];
+
+const directions = [
+  [-1, 2],
+  [-2, 1],
+  [1, 2],
+  [2, 1],
+  [-2, -1],
+  [-1, -2],
+  [2, -1],
+  [1, -2]
+]
+
+let idx = 1;
+for(let t = 0; t < T; t++) {
+  const l = Number(input[idx++]);
+  const [sx, sy] = input[idx++].split(' ').map(Number);  // start
+  const [ex, ey] = input[idx++].split(' ').map(Number);  // end
+  
+  const dist = Array.from({ length: l }, () => Array(l).fill(-1));
+  const queue = [];
+  dist[sx][sy] = 0;
+  queue.push([sx, sy]);
+  
+  let head = 0;
+  while(head < queue.length) {
+    const [x, y] = queue[head++];
+    
+    for(let [dx, dy] of directions) {
+      const nx = x + dx;
+      const ny = y + dy;
+      
+      if(nx < 0 || ny < 0 || nx >= l || ny >= l) continue;
+      if(dist[nx][ny] !== -1) continue;
+      
+      dist[nx][ny] = dist[x][y] + 1;
+      queue.push([nx, ny]);
+    }
+  }
+  
+  answer.push(dist[ex][ey]);
+}
+
+console.log(answer.join('\n'));
