0a5f58b9a114400929871c5399d03dbb91944010
Jeongmin Oh <51373392+ojm51@users.noreply.github.com>
2026-01-19 14:45:38 +0900

    [Silver III] Title: 1로 만들기, Time: 168 ms, Memory: 17448 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.js
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.js
@@ -4,13 +4,9 @@ const N = Number(input);
 const dp = Array(N + 1).fill(0);
 
 for(let i = 2; i <= N; i++) {
-  const calc = [];
-  
-  if(i % 3 === 0) calc.push(dp[i / 3]);
-  if(i % 2 === 0) calc.push(dp[i / 2]);
-  calc.push(dp[i - 1]);
-  
-  dp[i] = Math.min(...calc) + 1;
+  dp[i] = dp[i - 1] + 1;
+  if(i % 3 === 0) dp[i] = Math.min(dp[i], dp[i / 3] + 1);
+  if(i % 2 === 0) dp[i] = Math.min(dp[i], dp[i / 2] + 1);
 }
 
 console.log(dp[N]);
